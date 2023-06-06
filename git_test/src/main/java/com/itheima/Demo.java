62320204b80f2664978b62a963e290f54b114106
hyd1128 <104418875+hyd1128@users.noreply.github.com>
2023-06-06 13:51:51 +0800

    Update Demo.java

--- git_test/src/main/java/com/itheima/Demo.java
+++ git_test/src/main/java/com/itheima/Demo.java
@@ -11,5 +11,6 @@ public class Demo {
         System.out.println("123");
         System.out.println("123");
         System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
